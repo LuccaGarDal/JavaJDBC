dc561e8e9028c7f185d49996760dff1121239291
LuccaGarDal <luccagarciarosa@gmail.com>
2025-11-10 15:47:01 -0300

    feat: MYsql connection (findAll and findByName)

--- src/main/java/academy/devdojo/ZZijdbc/service/ProducerService.java
+++ src/main/java/academy/devdojo/ZZijdbc/service/ProducerService.java
@@ -31,4 +31,10 @@ public class ProducerService {
     public static List<Producer> findAll () {
         return ProducerRepository.findAll();
     }
+
+    public static List<Producer> findByName (String name) {
+        return ProducerRepository.findByName(name);
+    }
+
+
 }
