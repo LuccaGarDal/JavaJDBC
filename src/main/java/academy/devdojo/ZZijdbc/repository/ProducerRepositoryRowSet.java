fa19b3fc8e7bb266139db43220034344da1b3036
LuccaGarDal <luccagarciarosa@gmail.com>
2025-11-18 20:27:38 -0300

    feat: MYsql connection (JDBC RowSet Update)

--- src/main/java/academy/devdojo/ZZijdbc/repository/ProducerRepositoryRowSet.java
+++ src/main/java/academy/devdojo/ZZijdbc/repository/ProducerRepositoryRowSet.java
@@ -2,6 +2,7 @@ package academy.devdojo.ZZijdbc.repository;
 
 import academy.devdojo.ZZijdbc.conn.ConnectionFactory;
 import academy.devdojo.ZZijdbc.dominio.Producer;
+import academy.devdojo.ZZijdbc.listener.CustomRowSetListener;
 
 import javax.sql.rowset.JdbcRowSet;
 import java.sql.SQLException;
@@ -14,6 +15,7 @@ public class ProducerRepositoryRowSet {
         String sql = "SELECT * from anime_store.producer WHERE name like ?;";
         List<Producer> producers = new ArrayList<>();
         try (JdbcRowSet jrs = ConnectionFactory.getJdbcRowSet()) {
+            jrs.addRowSetListener(new CustomRowSetListener());
             jrs.setCommand(sql);
             jrs.setString(1, String.format("%%%s%%", name));
             jrs.execute();
@@ -29,4 +31,22 @@ public class ProducerRepositoryRowSet {
         }
         return producers;
     }
+
+    public static void updateJdbcRowSet (Producer producer) {
+        String sql = "SELECT * FROM anime_store.producer WHERE (id = ?);";
+        try (JdbcRowSet jrs = ConnectionFactory.getJdbcRowSet()) {
+            jrs.addRowSetListener(new CustomRowSetListener());
+            jrs.setCommand(sql);
+            jrs.setInt(1, producer.getId());
+            jrs.execute();
+            if (!jrs.next()) return;
+            jrs.updateString("name", producer.getName());
+            jrs.updateRow();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
 }
