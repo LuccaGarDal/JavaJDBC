a1aa5b72c83894c2002dd12b4415c416b01f94a7
LuccaGarDal <luccagarciarosa@gmail.com>
2025-11-19 15:25:21 -0300

    feat: MYsql connection (JDBC Disconnected RowSet)

--- src/main/java/academy/devdojo/ZZijdbc/repository/ProducerRepositoryRowSet.java
+++ src/main/java/academy/devdojo/ZZijdbc/repository/ProducerRepositoryRowSet.java
@@ -4,10 +4,13 @@ import academy.devdojo.ZZijdbc.conn.ConnectionFactory;
 import academy.devdojo.ZZijdbc.dominio.Producer;
 import academy.devdojo.ZZijdbc.listener.CustomRowSetListener;
 
+import javax.sql.rowset.CachedRowSet;
 import javax.sql.rowset.JdbcRowSet;
+import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class ProducerRepositoryRowSet {
 
@@ -44,6 +47,25 @@ public class ProducerRepositoryRowSet {
             jrs.updateRow();
 
         } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public static void updateCachedRowSet (Producer producer) {
+        String sql = "SELECT * FROM producer WHERE (id = ?);";
+        try (CachedRowSet crs = ConnectionFactory.getCachedRowSet();
+             Connection connection = ConnectionFactory.getConnection();) {
+            connection.setAutoCommit(false);
+            crs.setCommand(sql);
+            crs.setInt(1, producer.getId());
+            crs.execute(connection);
+            if (!crs.next()) return;
+            crs.updateString("name", producer.getName());
+            crs.updateRow();
+            TimeUnit.SECONDS.sleep(10);
+            crs.acceptChanges();
+        } catch (SQLException | InterruptedException e) {
             throw new RuntimeException(e);
         }
 
