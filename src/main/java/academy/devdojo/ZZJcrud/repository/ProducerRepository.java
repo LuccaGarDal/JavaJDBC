2fc66ee54fad4430bb0b1e88691d16f547914025
LuccaGarDal <luccagarciarosa@gmail.com>
2025-11-24 18:05:20 -0300

    Crud (save)

--- src/main/java/academy/devdojo/ZZJcrud/repository/ProducerRepository.java
+++ src/main/java/academy/devdojo/ZZJcrud/repository/ProducerRepository.java
@@ -5,10 +5,7 @@ import academy.devdojo.ZZJcrud.conn.ConnectionFactory;
 import academy.devdojo.ZZJcrud.dominio.Producer;
 import lombok.extern.log4j.Log4j2;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -59,6 +56,24 @@ public class ProducerRepository {
         ps.setInt(1, id);
         return ps;
     }
+
+    public static void save(Producer producer) {
+        log.info("Saving producer {}", producer);
+        try (Connection conn = ConnectionFactory.getConnection();
+             PreparedStatement ps = createPreparedStatementSave(conn, producer)) {
+            ps.execute();
+        } catch (SQLException e) {
+            log.error("Error while trying to insert producer {}", producer.getName(), e);
+        }
+    }
+
+    private static PreparedStatement createPreparedStatementSave(Connection conn, Producer producer) throws SQLException {
+        String sql = "INSERT INTO `anime_store`.`producer` (`name`) VALUES (?);";
+        PreparedStatement ps = conn.prepareStatement(sql);
+        ps.setString(1, producer.getName());
+        return ps;
+    }
+
 
 
 }
--- src/main/java/academy/devdojo/ZZJcrud/service/ProducerService.java
+++ src/main/java/academy/devdojo/ZZJcrud/service/ProducerService.java
@@ -3,7 +3,6 @@ package academy.devdojo.ZZJcrud.service;
 import academy.devdojo.ZZJcrud.dominio.Producer;
 import academy.devdojo.ZZJcrud.repository.ProducerRepository;
 
-import java.util.List;
 import java.util.Scanner;
 
 public class ProducerService {
@@ -11,14 +10,10 @@ public class ProducerService {
 
     public static void menu(int op) {
         switch (op) {
-            case 1:
-                findByName();
-                break;
-            case 2:
-                delete();
-                break;
-            default:
-                throw new IllegalArgumentException("Not a valid option");
+            case 1 -> findByName();
+            case 2 -> delete();
+            case 3 -> save();
+            default -> throw new IllegalArgumentException("Not a valid option");
         }
     }
 
@@ -33,10 +28,19 @@ public class ProducerService {
     public static void delete() {
         System.out.println("Type the id of the producer you want to delete");
         int id = Integer.parseInt(SCANNER.nextLine());
-        System.out.println("Are you sure? S/N");
+        System.out.println("Are you sure? Y/N");
         String choice = SCANNER.nextLine();
-        if ("s".equalsIgnoreCase(choice)) {
+        if ("y".equalsIgnoreCase(choice)) {
             ProducerRepository.delete(id);
         }
     }
+
+    public static void save() {
+        System.out.println("Type the name of the producer");
+        String name = SCANNER.nextLine();
+        Producer producer = Producer.builder().name(name).build();
+        ProducerRepository.save(producer);
+    }
+
+
 }
